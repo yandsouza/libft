eb54b0bd5c717edd594c6c07e3ead276cde60512
Yann D'Souza <yannssouza@outlook.com>
2026-06-05 10:19:11 -0300

    makefile correction

--- ft_strnstr.c
+++ ft_strnstr.c
@@ -6,7 +6,7 @@
 /*   By: ynascime <yannssouza@outlook.com>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/05/20 15:37:21 by ynascime          #+#    #+#             */
-/*   Updated: 2026/06/01 16:55:06 by ynascime         ###   ########.fr       */
+/*   Updated: 2026/06/04 14:04:32 by ynascime         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,25 +17,23 @@ char	*ft_strnstr(const char *big, const char *little, size_t len)
 	size_t	to_find_len;
 	size_t	i;
 
-	if (!little || !big)
-		return (NULL);
-	to_find_len = ft_strlen(little);
-	if (!to_find_len)
-		return ((char *)big);
 	i = 0;
+	to_find_len = ft_strlen(little);
+	if (to_find_len == 0)
+		return ((char *)big);
 	while (big[i] != '\0' && i < len)
 	{
 		if (i + to_find_len > len)
-			return (NULL);
+			return (0);
 		if (ft_strncmp(big + i, little, to_find_len) == 0)
 			return ((char *)big + i);
 		i++;
 	}
-	return (NULL);
+	return (0);
 }
-//#include <stdio.h>
-//int	main(void)
-//{
-//	printf("%s\n", ft_strnstr("Hello", "ell", 5));
-//	return (0);
-//}
+#include <stdio.h>
+int	main(void)
+{
+	printf("%s\n", ft_strnstr("Hello", "ell", 5));
+	return (0);
+}
